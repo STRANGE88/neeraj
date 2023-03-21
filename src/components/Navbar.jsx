1ef9abdc58aaf437222097dd54006b311a4ba8e0
STRANGE88 <127194751+STRANGE88@users.noreply.github.com>
2023-03-21 15:48:34 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div>
+         <Box sx={{flexGrow: 1 }}>
+         <AppBar position="static"> 
+     <Toolbar>
+             <Typography>HELLO</Typography>
+            <Button color='inherit' ><Link to="/read" style={{color:'white'}}>Students</Link></Button>
+         <Button color='inherit' ><Link to="/ssss" style={{color:'white'}}>Teachers</Link></Button>
+        </Toolbar>
+          </AppBar>
+       </Box>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Ssss.jsx
@@ -0,0 +1,49 @@
+import { Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import axios from 'axios';
+
+const Ssss = () => {
+    var [st, setst] = useState([])
+    useEffect(() => {
+        axios.get("http://localhost:3005/teacher")
+            .then(response => {
+                setst(st = response.data);
+            })
+            .catch(err => console.log(err))
+    }, []);
+    return (
+        <div>
+            <Typography>WELCOME TO MY APP</Typography>
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell >Id</TableCell>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Grade</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {st.map((value,index)=>{
+                            return<TableRow key={index}>
+                                <TableCell>{value.id}</TableCell>
+                                <TableCell>{value.name}</TableCell>
+                                <TableCell>{value.class}</TableCell>
+                            </TableRow>
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
+
+export default Ssss
